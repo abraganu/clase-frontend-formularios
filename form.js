c9a7f4f0c7fdcdcd00bfcd55e2197beeb1181a7e
Abraham Garcia <abraham.garcia@encora.com>
2021-03-01 21:19:32 -0600

    se agrego funcion para insertar mensaje de error

--- form.js
+++ form.js
@@ -1,17 +1,53 @@
-const inputName = document.querySelector('#name')
+const inputName = document.getElementById('name')
 const inputEmail = document.querySelector('#email')
 
 inputName.addEventListener('blur', function (event) {
-    console.log(inputName.validity)
+    if (inputName.validity.valid) {
+        inputName.classList.replace('is-invalid', 'is-valid')
+    }
+
     if (inputName.validity.valueMissing) {
-        console.log('el campo es requerido')
+        // ejecucion de funcion
+        addErrorMessage('El campo es requerido', inputName)
     }
 
     if (inputName.validity.tooShort) {
-        console.log('el minimo de caracteres es 2')
+        addErrorMessage('El minimo de caracteres es 2', inputName)
     }
 })
 
 inputEmail.addEventListener('blur', function () {
-    console.log('soy un blur en email')
+    const isInvalidClassInTheElement = inputEmail.classList.replace('is-invalid', 'is-valid')
+    if (inputEmail.validity.valid)  {
+        if (isInvalidClassInTheElement) {
+            inputEmail.classList.replace('is-invalid', 'is-valid')
+        } else {
+            inputEmail.classList.add('is-valid')
+        }
+    }
+
+    if (inputEmail.validity.valueMissing) {
+        addErrorMessage('El campo es requerido', inputEmail)
+    }
+
+    if (inputEmail.validity.typeMismatch) {
+        addErrorMessage('El formato de email no es correcto', inputEmail)
+    }
 })
+
+// declaracion de funcion
+// parametro: errorMessage
+// inputName.parentNode.removeChild()
+// inputName.sib
+function addErrorMessage(errorMessage, element) {
+    element.classList.add('is-invalid')
+    if (element.nextElementSibling) {
+        // remueve el div del error
+        element.parentNode.removeChild(element.nextSibling)
+    }
+
+    element.insertAdjacentHTML(
+        'afterend',
+        `<div class="invalid-feedback">${errorMessage}</div>`
+    )
+}
